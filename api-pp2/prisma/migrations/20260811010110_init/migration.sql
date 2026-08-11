aaa07bec45f208d5ff1335d1826123f31eda0f02
Yasmim Alves Ferreira <yasmimalsferreira@gmail.com>
2026-08-10 22:10:54 -0300

    aula nova

---
+++ api-pp2/prisma/migrations/20260811010110_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
